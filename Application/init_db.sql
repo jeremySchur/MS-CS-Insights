1d650d20060b3ad99e3162a2bc2e43c0fa021fe9
sesh9096 <89329755+sesh9096@users.noreply.github.com>
2024-10-17 21:16:23 +0000

    implement changes discussed in pr

--- Application/init_db.sql
+++ Application/init_db.sql
@@ -2,19 +2,20 @@
 \c postgres
 
 -- Create table of Channels
-CREATE TABLE IF NOT EXISTS channel_sentiments (
-    channel_id VARCHAR(13) PRIMARY KEY, -- Channel_id with max length of 13 characters
-    avg_sentiment DECIMAL(3, 2) -- Average sentiment score between -1.00 and 1.00
+CREATE TABLE IF NOT EXISTS channel (
+    id VARCHAR(13) PRIMARY KEY, -- Channel_id with max length of 13 characters
+    avg_sentiment DECIMAL(3, 2), -- Average sentiment score between -1.00 and 1.00
+    last_read TIMESTAMP(6), -- Timestamp of last read message
+    name VARCHAR(80) --slack channels can only be 80 characters long
 );
 
--- Create table of Messages for each Channel
-CREATE TABLE IF NOT EXISTS messages_sentiments (
-    message_ts TIMESTAMP(3) PRIMARY KEY, -- Timestamp with milliseconds percision
-    message_text TEXT,
-    sentiment_score DECIMAL(3, 2), -- Sentiment score between -1.00 and 1.00
-    processed BOOLEAN, -- Indicates if this message has been analyzed
-    channel_id TEXT,
-    FOREIGN KEY (channel_id) REFERENCES channel_sentiments(channel_id)
+-- Create table of Messages
+CREATE TABLE IF NOT EXISTS message (
+    ts TIMESTAMP(6) PRIMARY KEY, -- probably unique
+    text TEXT,
+    sentiment DECIMAL(3, 2), -- Sentiment score between -1.00 and 1.00
+    user_id VARCHAR(13),
+    channel_id VARCHAR(13) REFERENCES channel(id)
 );
 
 
