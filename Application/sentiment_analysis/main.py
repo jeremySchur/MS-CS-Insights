ea884bdd1c74d38940ac3800b9620ed75371c328
Jeremy Schur <jeremy.schur02@gmail.com>
2024-10-20 16:25:52 -0600

    updated main.py to use schedules

--- Application/sentiment_analysis/main.py
+++ Application/sentiment_analysis/main.py
@@ -2,26 +2,37 @@ from slack import update_public_channels, get_all_messages
 from analysis import analyze_sentiments
 from postgres import insert_messages, update_timestamps, get_channels, update_avg_sentiments
 from time import sleep
+import schedule
 import json
+
+def job():
+    """
+        Main function to run the message fetching and sentiment analysis
+        :param None
+        :return: None
+    """
+    # Get all public channels
+    channels = get_channels()
+    # Update the public channels
+    update_public_channels(channels)
+    # print(json.dumps(channels, indent=4)) # Uncomment to see all channels
+
+    # Get all messages from the public channels
+    messages = get_all_messages(channels)
+    analyze_sentiments(messages)
+    # put messages in to db
+    insert_messages(messages)
+    # update timestamps to latest
+    update_timestamps(channels)
+    # update average sentiments
+    update_avg_sentiments()
+
+    # print(json.dumps(messages, indent=4)) # Uncomment to see all messages
+    return None
 
 # Main function
 if __name__ == '__main__':
-    # Get all public channels
-    channels = get_channels()
+    schedule.every(60).seconds.do(job)   
     while True:
-        # Update the public channels
-        update_public_channels(channels)
-        # print(json.dumps(channels, indent=4)) # Uncomment to see all channels
-        # Get all messages from the public channels
-        messages = get_all_messages(channels)
-        analyze_sentiments(messages)
-        # put messages in to db
-        insert_messages(messages)
-        # update timestamps to latest
-        update_timestamps(channels)
-        # update average sentiments
-        update_avg_sentiments()
-
-        print(json.dumps(messages, indent=4))
-        sleep(60)  # Sleep for 60 seconds
-
+        schedule.run_pending()
+        sleep(1)
